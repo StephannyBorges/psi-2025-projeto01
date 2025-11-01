31c45365993cb8827d03fa5dd62111d00ec18eaa
stephanny <stephannyborgestete@gmail.com>
2025-10-31 22:24:24 -0300

    CRUD Forms

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-       path('jogadores/', views.jogadores, name='jogadores'),
+    #path('jogadores/', views.jogadores, name='jogadores'),
+    path('jogadores/', views.lista_jogadores, name='lista_jogadores'),
+    path('criar_jogador/', views.criar_jogador, name='criar_jogador'),
+    path('detalhe_jogador/<int:pk>/', views.detalhe_jogador, name='detalhe_jogador'),
+    path('editar_jogador/<int:pk>/', views.editar_jogador, name='editar_jogador'),
+    path('excluir_jogador/<int:pk>/', views.excluir_jogador, name='excluir_jogador'),
     path('sobre/', views.sobre, name='sobre'),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Jogador
+from .forms import JogadorForm
 
 
 def index(request):
     return render(request, "index.html")
 
-def jogadores(request):
+#def jogadores(request):
+ #   jogadores = Jogador.objects.all()
+  #  return render(request, "jogadores.html", {"jogadores": jogadores})
+
+def lista_jogadores(request):
     jogadores = Jogador.objects.all()
-    return render(request, "jogadores.html", {"jogadores": jogadores})
+    return render(request, "jogadores/lista.html", {"jogadores": jogadores})
+
+def detalhe_jogador(request, pk):
+    jogador = get_object_or_404(Jogador, pk=pk)
+    return render(request, 'jogadores/detalhe.html', {'jogador': jogador})
+
+def criar_jogador(request):
+    if request.method == "POST":
+        form = JogadorForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_jogadores')
+    else:
+        form = JogadorForm()
+    return render(request, "jogadores/formulario.html", {"form": form})
+
+def editar_jogador(request, pk):
+    jogador = get_object_or_404(Jogador, pk=pk)
+
+    if request.method == "POST":
+        form = JogadorForm(request.POST, instance=jogador)
+        if form.is_valid():
+            form.save()
+            return redirect("lista_jogadores")
+    else:
+        form = JogadorForm(instance=jogador)
+
+    return render(request, "jogadores/formulario.html", {"form": form})
+
+def excluir_jogador(request, pk):
+    jogador = get_object_or_404(Jogador, id=pk)
+    if request.method == "POST":
+        jogador.delete()
+        return redirect('lista_jogadores')
+    return render(request, "jogadores/excluir.html", {"jogador": jogador})
 
 def sobre(request):
     return render(request, "sobre.html")
